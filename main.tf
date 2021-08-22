eb2607bbad292814bccd3b6ea2d68a1d720caf07
venu <venugopal.k1994@gmail.com>
2021-08-22 15:22:27 -0400

    adding venu SG for venu_1

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "venu-instance" {
  ami = "ami-02f84cf47c23f1769"
  instance_type = "t2.micro"
+ security_groups = ["${aws_security_group.venu_terraform.name}"]
  
  tags = {
      Name = "venu_1"
@@ -32,5 +33,25 @@ resource "aws_security_group" "terraform" {
   
   tags = {
     Name = "terraform"
+  }
+}
+
+resource "aws_security_group" "venu_terraform" {
+  name = "venu_securityGroup"
+  description = "venu_1 security group"
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "security_group"
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self = false
+    } ]
+  
+  tags = {
+    Name = "venu_securitygroup"
   }
 }
