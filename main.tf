5cca2697957563087e1fdcaf4832432f49ba626c
Rahul Odela <rahulodela99@gmail.com>
2021-08-21 20:19:18 -0400

    security group

--- main.tf
+++ main.tf
@@ -9,8 +9,28 @@ resource "aws_instance" "venu-instance" {
 resource "aws_instance" "rahul-instance" {
  ami = "ami-02f84cf47c23f1769"
  instance_type = "t2.micro"
+ security_groups = ["${aws_security_group.terraform}"]
  
  tags = {
      Name = "rahul_1"
  } 
+}
+resource "aws_security_group" "terrafrom" {
+  name = "instance security"
+  description = "rahul security group"
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "security_group"
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self = false
+    } ]
+  
+  tags = {
+    Name = "terraform"
+  }
 }
